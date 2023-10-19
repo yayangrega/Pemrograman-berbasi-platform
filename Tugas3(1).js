7e9c53e44215945f199d6dfca6ac76f2863af148
Yayangregaabdilah <yayang.abdilah_ti22@nusaputra.ac.id>
2023-10-20 02:36:29 +0700

    first commit

---
+++ Tugas3(1).js
@@ -0,0 +1,24 @@
+let restaurantObject = {
+    Name: "Cafe Buana",
+    City: "Bandung",
+    Favorite_drink: "Capuchino",
+    Favorite_food: "Nasi Goreng Ati Ayam",
+    isVegan: false
+};
+
+let restaurantArray = [
+ {
+    Name: "Cafe Buana",
+    City: "Bandung",
+    Favorite_drink: "Capuchino",
+    Favorite_food: "Nasi Goreng Ati Ayam",
+    isVegan: false
+ },
+ {
+    Name: "Sono coffe",
+    City: "Sukabumi",
+    Favorite_drink: "Caramel Machiato",
+    Favorite_food: "Dimsum",
+    isVegan: true
+ }
+];
